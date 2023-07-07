b07be825be5cc98cea36d59fbc12025a779986ec
Adam <92753806+AdamUbel@users.noreply.github.com>
2023-07-06 23:34:06 -0700

    Added comments/notes

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ import { gridDefault, makeWordSet, getRandomWord, gridColorDefault } from "./Wor
 export const AppContext = createContext();
 
 function App() {
-  const [grid, setGrid] = useState(gridDefault);
-  const [gridColor, setGridColor] = useState(gridColorDefault);
-  const [currPosition, setCurrPosition] = useState({ selectedRow: 0, selectedLetter: 0 });
-  const [usedKeys, setUsedKeys] = useState([]);
-  const [correctKeys, setCorrectKeys] = useState([]);
-  const [containsKeys, setContainsKeys] = useState([]);
-  const [wordBankSet, setWordBankSet] = useState(new Set());
-  const [theWord, setTheWord] = useState("WORDS");
-  const [guessedWord, setGuessedWord] = useState("");
-  const [guesses, setGuesses] = useState([]);
+  // State variables
+  const [grid, setGrid] = useState(gridDefault); // Grid state
+  const [gridColor, setGridColor] = useState(gridColorDefault); // Grid color state
+  const [currPosition, setCurrPosition] = useState({ selectedRow: 0, selectedLetter: 0 }); // Current position state
+  const [usedKeys, setUsedKeys] = useState([]); // Used keys state
+  const [correctKeys, setCorrectKeys] = useState([]); // Correctly guessed keys state
+  const [containsKeys, setContainsKeys] = useState([]); // Keys contained in the word state
+  const [wordBankSet, setWordBankSet] = useState(new Set()); // Set of words in the word bank state
+  const [theWord, setTheWord] = useState("WORDS"); // The target word state
+  const [guessedWord, setGuessedWord] = useState(""); // The currently guessed word state
+  const [guesses, setGuesses] = useState([]); // List of previous guesses state
 
-  const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false); // Modal visibility state
 
   useEffect(() => {
+    // Effect to initialize the word set and select a random word when the selected row is 0
     if (currPosition.selectedRow === 0) {
       makeWordSet().then((words) => {
         setWordBankSet(words.wordSet);
@@ -31,6 +33,7 @@ function App() {
       });
     }
 
+    // Effect to show the outcome modal when all keys are correct or the selected row exceeds 5
     if (correctKeys.length === 5 || currPosition.selectedRow > 5) {
       setShowModal(true);
     }
@@ -65,11 +68,11 @@ function App() {
         }}
       >
         <div className={showModal ? "game-container blurred" : "game-container"}>
-          <Grid />
-          <Keyboard />
+          <Grid /> {/* Renders the Grid component */}
+          <Keyboard /> {/* Renders the Keyboard component */}
         </div>
 
-        {/* Outcome modal  NOTE */}
+        {/* Outcome modal */}
         <div className={showModal ? "modal" : "hidden"}>
           <h1>Results</h1>
           <h2>

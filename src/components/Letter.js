6c8e25153017a0feb73e9d2141c73f6d081033ff
Adam <92753806+AdamUbel@users.noreply.github.com>
2023-07-07 20:58:48 -0700

    Added notes/comments

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
-
 import { AppContext } from "../App";
 
+// Letter component
 const Letter = ({ letterPos, attempVal }) => {
+  // Accessing relevant states from AppContext using useContext hook
   const { grid, gridColor, currPosition } = useContext(AppContext);
   const { selectedRow } = currPosition;
 
+  // Getting the letter and its color from the grid and gridColor arrays
   const letter = grid[attempVal][letterPos];
-
   const letterColor = gridColor[attempVal][letterPos];
 
   return (
